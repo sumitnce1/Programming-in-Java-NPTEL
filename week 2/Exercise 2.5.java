d118fe9c0ac51246390ac5249d005b78ada9a34f
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-08-22 09:52:55 +0530

    Add files via upload

---
+++ week 2/Exercise 2.5.java
@@ -0,0 +1,12 @@
+//To debug the program which is intended to print 'NPTEL JAVA'.
+public class Question215{ 
+    public static void main(String[] args) { 
+	//Declare variable with name 'nptel', 'space' and 'java' and proper datatype.
+	String nptel,space,java;
+	//Initialize the variables with proper input
+	nptel="NPTEL";
+	space=" ";
+	java="JAVA";
+     	System.out.print(nptel+space+java);
+   }
+}

2cf62fb0e2ac738330911d91ecd2997c1f6a1b9e
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-08-22 09:51:44 +0530

    Add files via upload

---
+++ Exercise 2.4.java
@@ -0,0 +1,16 @@
+//To call default constructor first and then any other constructor in the class Answer.
+// This is the main class Question
+public class Question214{
+	public static void main(String[] args){
+		Answer a = new Answer(10,"MCQ");
+	}
+} 
+class Answer{
+	Answer(){
+		System.out.println("You got nothing.");
+	}
+	Answer(int marks, String type){
+		this();
+		System.out.println("You got "+marks+" for an "+ type);
+	}
+} 

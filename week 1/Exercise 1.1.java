2c5b1a2d9c5a8dd40b0b7d0822b514ecd717d7e7
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-08-22 09:48:24 +0530

    1st week NPTEL Answer

---
+++ week 1/Exercise 1.1.java
@@ -0,0 +1,18 @@
+//To find the perimeter and area of a circle given a value of radius.
+import java.util.Scanner;  
+public class Exercise1_1 {
+       public static void main(String[] args) {
+Scanner s = new Scanner(System.in); 
+       double radius= s.nextDouble();
+       double perimeter;
+       double area;
+//Calculate the perimeter 
+
+	perimeter = 2 * Math.PI * radius;
+//Calculate the area
+	area = perimeter * radius;
+	
+	System.out.println(perimeter);
+	System.out.println(area);
+	}
+}
---
+++ week 1/Exercise 1.3.java
@@ -0,0 +1,19 @@
+// Consider First n even numbers starting from zero(0) and calculate sum of all the numbers divisible by 3 from 0 to n. Print the sum.
+import java.util.Scanner;
+public class Exercise1_3 {
+       public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0,t;
+ //Use for or while loop do the operation
+		for(int i=0;i<=n;i++)
+		{
+			t = 2*(i-1);
+			if(t%3 == 0)
+			{
+				sum += t;
+			}
+		}
+		System.out.println(sum);
+	}
+}

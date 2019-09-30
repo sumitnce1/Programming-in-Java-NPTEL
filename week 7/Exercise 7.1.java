daa88dddbf5446b5edba343e71397fbf1c871f0c
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-09-30 10:38:12 +0530

    Add files via upload

---
+++ week 7/Exercise 7.1.java
@@ -0,0 +1,14 @@
+// Write the appropriate statement(s) to import the package(s) you need in your program
+import java.util.*;
+public class Question1{ 
+        public static void main (String[] args){
+
+//Write the appropriate code to read the 3 integer values and find their sum.
+Scanner obj = new Scanner(System.in);
+         int a =  obj.nextInt();
+         int b =  obj.nextInt();
+         int c =  obj.nextInt();
+         int sum = a+b+c;
+ System.out.println(sum);
+  }
+}

6886bbac3bb74b11c580e67a54dff49dfa950614
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-10-11 11:43:21 +0530

    Add files via upload

---
+++ week 10/Exercise 10.1.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class Question101 {
+    public static void main(String args[]) {
+        try {
+              Connection conn = null;
+              Statement stmt = null;
+              String DB_URL = "jdbc:sqlite:/tempfs/db";
+              System.setProperty("org.sqlite.tmpdir", "/tempfs");
+              
+              // JDBC Codes in the hidden section
+              
+              
+~~~THERE IS SOME INVISIBLE CODE HERE~~~
+            }
+       catch(Exception e){ System.out.println(e);}  
+    }
+}

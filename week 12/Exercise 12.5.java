75ccfa0fc3e1bb507eec66d19be4f29c59835bef
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-10-17 22:10:07 +0530

    Add files via upload

---
+++ week 12/Exercise 12.5.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Question5 { 
+    static int sum_odd_n(int n){ 
+          if(n==1)
+              return 1;
+           if (n <= 0) 
+                return 0;
+                
+                
+                
+                 int nce=n,sumit=0;
+ 
+  if(nce==n)  
+    {
+  sumit=n*2-1;
+ }
+
+   return sumit+sum_odd_n(n-1);
+   
+   
+   
+   
+   
+   
+  }
+   public static void main(String[] args) {  
+      Scanner in=new Scanner(System.in);
+      int count=in.nextInt();      
+      int r=sum_odd_n(count);
+      System.out.println(r); 	  
+    }  
+}

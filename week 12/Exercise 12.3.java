26914b67b3f1a06b75797b145e44e6cddebe9e6e
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-10-17 22:08:54 +0530

    Add files via upload

---
+++ week 12/Exercise 12.3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Question3{
+  public static void main(String[] args){
+      Scanner s1 = new Scanner(System.in);
+      
+      
+      String sumitnce=s1.nextLine();
+String nce=s1.nextLine();
+
+int roll=s1.nextInt();
+
+double s1nce=s1.nextDouble();
+double s2nce=s1.nextDouble();
+
+double avg=(s1nce+s2nce)/2;
+
+System.out.print(sumitnce+nce+" "+roll+" "+avg);
+
+    }
+}

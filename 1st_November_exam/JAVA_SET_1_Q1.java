e755b485379fd8c9d0e6ae105e260b95cc63f10a
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-11-01 10:40:19 +0530

    Add files via upload

---
+++ 1st_November_exam/JAVA_SET_1_Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Question1{
+	public static void main(String args[]){
+        int n;
+        
+        ~~~THERE IS SOME INVISIBLE CODE HERE~~~
+        
+        // write code from below...
+System.out.print((n*(n+1)));
+        
+
+
+	} // main() ends here
+} // class Question1 ends here

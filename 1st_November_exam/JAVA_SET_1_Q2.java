c68535a67791139e3429490b776aee371b1b1bb4
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-11-01 10:40:40 +0530

    Add files via upload

---
+++ 1st_November_exam/JAVA_SET_1_Q2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Question2{
+	public static void main(String args[]){
+        int arr[] = new int[20];
+
+        // Array initialization is hidden
+        
+        ~~~THERE IS SOME INVISIBLE CODE HERE~~~
+        
+        int max = arr[0];
+
+for(int j=0 ; j<arr.length ; j++)
+  if(max < arr[j])
+  	max = arr[j];
+
+System.out.print(max);
+
+
+	} // main() ends here
+} // Class Question2 ends here
